6d606b7b2beddb92a81c5e6f974ab5df146b0c7d
mensagemParaVoce <mensagemparavoce_54@hotmail.com>
2022-09-04 11:47:57 -0300

    segundo teste com Git Hub HelloWorld!!

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,11 @@
 console.log("Hello World, gitHub!!!");
+console.log(teste());
+
+function teste(){
+  let i;
+  for(i = 0; i<1; i++){
+    if(i){
+      console.log(i);
+    }
+  }
+}
--- helloWorld.php
+++ helloWorld.php
@@ -1,3 +1,13 @@
 <?php
 
-print "Hello World, gitHub!!!";
+print "Hello World, gitHub!!!<br>";
+print teste();
+
+function teste(){
+  for($i=0; $i<5; $i++){
+    echo "teste";
+    if($i > -1){
+      echo $i."<br>";
+    }
+  }
+}
